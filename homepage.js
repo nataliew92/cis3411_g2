d78fc93480e1bb83f4686eac195ad0dded1e47f4
Greta <89580915+GretaIvanovaite@users.noreply.github.com>
2026-03-26 10:32:36 +0000

    Console log check for the API call

--- homepage.js
+++ homepage.js
@@ -1,7 +1,8 @@
 const apiBase = "https://api.vam.ac.uk/v2/objects/search?id_category=THES48967&id_collection=THES48593&images_exist=1&page_size=100&data_restrict=descriptive_only";
 
 async function fetchData() {
-    while (page < 11) {
+    let page = 1;
+    while (page < 9) {
         const URL = apiBase + '&page=' + page;
         try {
             const response = await fetch(URL);
@@ -10,10 +11,12 @@ async function fetchData() {
                 return [];
             }
             console.log(jsonData.records);
-            return jsonData.records;
+            page += 1;
         } catch (error) {
             return [];
         }
     }
     
 }
+
+fetchData();
