113bda99522e0c3fc2f98db045be1bccd70a35d6
Greta <89580915+GretaIvanovaite@users.noreply.github.com>
2026-04-02 15:45:01 +0100

    Added config placeholder until we can connect to netlify; created gitignore; storing object data for clustering

--- homepage.js
+++ homepage.js
@@ -3,7 +3,7 @@ const imageUrl = "https://framemark.vam.ac.uk/collections";
 const main = document.getElementById('homepage');
 
 async function fetchData() {
-    let allObjects =[];
+    let allObjects = [];
     let page = 1;
     while (page < 9) {
         const URL = apiBase + '&page=' + page;
@@ -19,44 +19,65 @@ async function fetchData() {
             return [];
         }
     }
-    const objectList = allObjects.flat();
-    console.log('Sample object:', objectList[0]);
-    return objectList;
-    
+    return allObjects.flat();
 }
 
-async function displayObjects() {
-    const objectList = await fetchData();
-    objectList.forEach(object => {
-        const card = document.createElement('article');
-        
-        /*title*/
+function buildObjectList(records) {
+    let objectList = [];
+    for (let i = 0; i < records.length; i++) {
+        let record = records[i];
+
         let title = '';
-        if (object._primaryTitle && object.objectType) {
-            title = object._primaryTitle + ' (' + object.objectType + ')';
-        } else if (object.objectType) {
-            title = object.objectType;
+        if (record._primaryTitle && record.objectType) {
+            title = record._primaryTitle + ' (' + record.objectType + ')';
+        } else if (record.objectType) {
+            title = record.objectType;
         } else {
             title = 'Untitled';
         }
 
-        /*image*/
-        const imgId = object._primaryImageId;
-        const image = imageUrl + "/" + imgId + "/full/!400,400/0/default.jpg";
+        let makerName = '';
+        if (record._primaryMaker) {
+            makerName = record._primaryMaker.name;
+        }
 
-        /*Adding to the HTML*/
+        let obj = {
+            systemNumber: record.systemNumber,
+            objectType: record.objectType,
+            title: title,
+            imageId: record._primaryImageId,
+            date: record._primaryDate,
+            place: record._primaryPlace,
+            maker: makerName,
+            cluster: null
+        };
+
+        objectList.push(obj);
+    }
+    return objectList;
+}
+
+async function displayObjects() {
+    const records = await fetchData();
+    const objectList = buildObjectList(records);
+
+    console.log('Total objects:', objectList.length);
+    console.log('Sample object:', objectList[0]);
+
+    for (let i = 0; i < objectList.length; i++) {
+        let obj = objectList[i];
+        const card = document.createElement('article');
+
+        const imgSrc = imageUrl + "/" + obj.imageId + "/full/!400,400/0/default.jpg";
+
         card.innerHTML =
-        '<h2>' + title + '</h2>' +
-        '<picture>' +
-            '<img src="' + image + '" alt="" width="600" height="600" loading="lazy">' +
-        '</picture>';
+            '<h2>' + obj.title + '</h2>' +
+            '<picture>' +
+                '<img src="' + imgSrc + '" alt="" width="400" height="400" loading="lazy">' +
+            '</picture>';
 
         main.appendChild(card);
-
-        /* Data stored for clustering - TODO */
-
-
-    });
+    }
 }
 
 displayObjects();
